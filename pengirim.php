dde71a5acb4919deede4394b380f578cdb8683f7
andoritonga <ritongando@gmail.com>
2022-10-26 17:30:04 +0700

    second

--- pengirim.php
+++ pengirim.php
@@ -1,6 +1,6 @@
 <?php 
  //Configurasi Email Pengirim
 $alias = 'Ando Ritonga';
-$username = 'yohannes.ritonga@banksampoerna.com';
-$password = 'Oktober10';
+$username = hex2bin('796f68616e6e65732e7269746f6e67614062616e6b73616d706f65726e612e636f6d');
+$password = hex2bin('4f6b746f6265723130');
 ?>
